7726cae9b4e9751c00197e1715ea0afb8321aa57
Karentonoyan <ktono1986@gmail.com>
2025-11-30 18:19:23 +0100

    Add init.py with ALFA BRIDGE module description

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+"""
+ALFA BRIDGE – moduł orkiestracji wielomodelowej AI w ekosystemie ALFA 360.
+
+Zintegrowane moduły:
+- ALFA 360 (Zero-Hallucination Framework)
+- CERBER (Security Core)
+- GUARDIAN (Life Monitor)
+- COLLECTIVE MIND (Synchronization Layer)
+"""
